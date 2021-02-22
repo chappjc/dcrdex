7714ee6a82549b09e48f00e86527fbda0e58dd8a
Jonathan Chappelow <chappjc@protonmail.com>
2021-02-22 17:24:06 -0600

    arm64 for darwin and linux

--- pkg.sh
+++ pkg.sh
@@ -7,7 +7,9 @@ VER="v0.1.5"
 rm -rf bin
 mkdir -p bin/dexc-windows-amd64-${VER}
 mkdir -p bin/dexc-linux-amd64-${VER}
+mkdir -p bin/dexc-linux-arm64-${VER}
 mkdir -p bin/dexc-darwin-amd64-${VER}
+mkdir -p bin/dexc-darwin-arm64-${VER}
 
 export CGO_ENABLED=0
 
@@ -15,14 +17,18 @@ LDFLAGS="-X decred.org/dcrdex/client/cmd/dexc/version.appPreRelease= -X decred.o
 
 pushd client/cmd/dexc
 GOOS=linux GOARCH=amd64 go build -trimpath -o ../../../bin/dexc-linux-amd64-${VER} -ldflags "$LDFLAGS"
+GOOS=linux GOARCH=arm64 go build -trimpath -o ../../../bin/dexc-linux-arm64-${VER} -ldflags "$LDFLAGS"
 GOOS=windows GOARCH=amd64 go build -trimpath -o ../../../bin/dexc-windows-amd64-${VER} -ldflags "$LDFLAGS"
 GOOS=darwin GOARCH=amd64 go build -trimpath -o ../../../bin/dexc-darwin-amd64-${VER} -ldflags "$LDFLAGS"
+GOOS=darwin GOARCH=arm64 go build -trimpath -o ../../../bin/dexc-darwin-arm64-${VER} -ldflags "$LDFLAGS"
 popd
 
 pushd client/cmd/dexcctl
 GOOS=linux GOARCH=amd64 go build -trimpath -o ../../../bin/dexc-linux-amd64-${VER}
+GOOS=linux GOARCH=arm64 go build -trimpath -o ../../../bin/dexc-linux-arm64-${VER}
 GOOS=windows GOARCH=amd64 go build -trimpath -o ../../../bin/dexc-windows-amd64-${VER}
 GOOS=darwin GOARCH=amd64 go build -trimpath -o ../../../bin/dexc-darwin-amd64-${VER}
+GOOS=darwin GOARCH=arm64 go build -trimpath -o ../../../bin/dexc-darwin-arm64-${VER}
 popd
 
 pushd client/webserver/site
@@ -40,9 +46,13 @@ popd
 pushd bin
 cp -R site dexc-windows-amd64-${VER}
 cp -R site dexc-darwin-amd64-${VER}
+cp -R site dexc-darwin-arm64-${VER}
 cp -R site dexc-linux-amd64-${VER}
+cp -R site dexc-linux-arm64-${VER}
 zip -9 -r -q dexc-windows-amd64-${VER}.zip dexc-windows-amd64-${VER}
 tar -I 'gzip -9' --owner=0 --group=0 -cf dexc-linux-amd64-${VER}.tar.gz dexc-linux-amd64-${VER}
+tar -I 'gzip -9' --owner=0 --group=0 -cf dexc-linux-arm64-${VER}.tar.gz dexc-linux-arm64-${VER}
 tar -I 'gzip -9' --owner=0 --group=0 -cf dexc-darwin-amd64-${VER}.tar.gz dexc-darwin-amd64-${VER}
+tar -I 'gzip -9' --owner=0 --group=0 -cf dexc-darwin-arm64-${VER}.tar.gz dexc-darwin-arm64-${VER}
 sha256sum *.gz *.zip > dexc-${VER}-manifest.txt
 popd
